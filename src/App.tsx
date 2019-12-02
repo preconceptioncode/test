44c8f03a63a844587b1b8597db1b777a88acabc7
Sean Aguinaga <aguinaga.sean@gmail.com>
2019-12-02 08:54:52 -0800

    Change Routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,37 @@
-import React from 'react';
-import { Redirect, Route } from 'react-router-dom';
-import { IonApp, IonRouterOutlet, IonSplitPane } from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
-import { AppPage } from './declarations';
-
-import Menu from './components/Menu';
-import Home from './pages/Home';
-import List from './pages/List';
-import { home, list } from 'ionicons/icons';
-
+import { IonApp, IonRouterOutlet, IonSplitPane } from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
 /* Core CSS required for Ionic components to work properly */
-import '@ionic/react/css/core.css';
-
+import "@ionic/react/css/core.css";
+import "@ionic/react/css/display.css";
+import "@ionic/react/css/flex-utils.css";
+import "@ionic/react/css/float-elements.css";
 /* Basic CSS for apps built with Ionic */
-import '@ionic/react/css/normalize.css';
-import '@ionic/react/css/structure.css';
-import '@ionic/react/css/typography.css';
-
+import "@ionic/react/css/normalize.css";
 /* Optional CSS utils that can be commented out */
-import '@ionic/react/css/padding.css';
-import '@ionic/react/css/float-elements.css';
-import '@ionic/react/css/text-alignment.css';
-import '@ionic/react/css/text-transformation.css';
-import '@ionic/react/css/flex-utils.css';
-import '@ionic/react/css/display.css';
-
+import "@ionic/react/css/padding.css";
+import "@ionic/react/css/structure.css";
+import "@ionic/react/css/text-alignment.css";
+import "@ionic/react/css/text-transformation.css";
+import "@ionic/react/css/typography.css";
+import { home, list } from "ionicons/icons";
+import React from "react";
+import { Route } from "react-router-dom";
+import Menu from "./components/Menu";
+import { AppPage } from "./declarations";
+import Home from "./pages/Home";
+import List from "./pages/List";
 /* Theme variables */
-import './theme/variables.css';
+import "./theme/variables.css";
 
 const appPages: AppPage[] = [
   {
-    title: 'Home',
-    url: '/home',
+    title: "Home",
+    url: "/",
     icon: home
   },
   {
-    title: 'List',
-    url: '/home/list',
+    title: "List",
+    url: "/list",
     icon: list
   }
 ];
@@ -47,9 +42,8 @@ const App: React.FC = () => (
       <IonSplitPane contentId="main">
         <Menu appPages={appPages} />
         <IonRouterOutlet id="main">
-          <Route path="/home" component={Home} exact={true} />
-          <Route path="/home/list" component={List} exact={true} />
-          <Route path="/" render={() => <Redirect to="/home" exact={true} /> } />
+          <Route path="/" component={Home} exact={true} />
+          <Route path="/list" component={List} exact={true} />
         </IonRouterOutlet>
       </IonSplitPane>
     </IonReactRouter>
